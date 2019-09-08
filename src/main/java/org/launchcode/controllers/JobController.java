9f3a2b125530f42dd47ffafab6c4761cd6392a3a
bobby <roberthughes1017@outlook.com>
2019-09-08 01:06:37 -0500

    Finished Project

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,12 +1,16 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import javax.validation.Valid;
 
@@ -25,6 +29,8 @@ public class JobController {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
 
+        model.addAttribute( "job",jobData.findById(id));
+
         return "job-detail";
     }
 
@@ -35,13 +41,25 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @ModelAttribute @Valid JobForm jobForm, Errors errors, RedirectAttributes attributes) {
 
         // TODO #6 - Validate the JobForm model, and if valid, create a
-        // new Job and add it to the jobData data store. Then
+        // new Job and add it to the jobData data store. The
         // redirect to the job detail view for the new Job.
+        if (errors.hasErrors()){
+            return "new-job";
+        }
+        Job newJob = new Job();
+        newJob.setName(jobForm.getName());
+        newJob.setEmployer(jobForm.getEmployer());
+        newJob.setLocation(jobForm.getLocation());
+        newJob.setCoreCompetency(jobForm.getCoreCompetency());
+        newJob.setPositionType(jobForm.getPositionType());
 
-        return "";
+        jobData.add(newJob);
+        attributes.addAttribute( "id",newJob.getId());
+
+        return "redirect:" ;
 
     }
 }
